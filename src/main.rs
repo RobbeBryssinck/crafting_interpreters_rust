eca0fad378a1a0e36c94d5f6642360f57da24dfa
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-06-23 15:21:33 +0200

    basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{env, process::exit};
+
+fn run_file(filename: &str) {
+    println!("Running file {filename}");
+}
+
+fn run_prompt() {
+    println!("Running prompt");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => run_prompt(),
+        2 => run_file(&args[1]),
+        _ => {
+            println!("Usage: jlox [script]");
+            exit(64);
+        }
+    }
 }
