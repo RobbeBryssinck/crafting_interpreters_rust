ee2df7b549e11dc4406273b6bc9626fddffaf0e7
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-08-13 21:38:57 +0200

    proper error propogation for interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -4,8 +4,8 @@ use crate::error_reporter;
 
 pub fn interpret(expr: &Expr) {
     let result = match evaluate(expr) {
-        Some(value) => value,
-        None => Value::Nil
+        Ok(value) => value,
+        Err(_e) => Value::Nil
     };
 
     if result == Value::Nil {
@@ -20,7 +20,7 @@ pub fn interpret(expr: &Expr) {
     error_reporter::reset_error();
 }
 
-fn evaluate(expr: &Expr) -> Option<Value> {
+fn evaluate(expr: &Expr) -> Result<Value, String> {
     match expr {
         Expr::Literal { value } => {
             return literal_to_value(value);
@@ -38,20 +38,20 @@ fn evaluate(expr: &Expr) -> Option<Value> {
                 TokenType::Minus => {
                     match right_object {
                         Value::Number(value) => {
-                            return Some(Value::Number(-value));
+                            return Ok(Value::Number(-value));
                         },
-                        _ => { return None; }
+                        _ => { return Err(generate_error(operator.line, "cannot apply '-' operator on a non-number.")); }
                     }
                 },
                 TokenType::Bang => {
                     match right_object {
                         Value::Bool(value) => {
-                            return Some(Value::Bool(!value));
+                            return Ok(Value::Bool(!value));
                         },
-                        _ => { return None; }
+                        _ => { return Err(generate_error(operator.line, "cannot apply '!' operator on a non-number.")); }
                     }
                 }
-                _ => { return None; }
+                _ => { return Err(generate_error(operator.line, "unary operator must be '-' or '!'.")); }
             }
         },
         Expr::Binary { 
@@ -67,106 +67,109 @@ fn evaluate(expr: &Expr) -> Option<Value> {
                     match (left_object, right_object) {
                         (Value::Number(left_value), Value::Number(right_value)) => 
                         {
-                            return Some(Value::Number(left_value - right_value));
+                            return Ok(Value::Number(left_value - right_value));
                         },
-                        (_, _) => { return None; }
+                        (_, _) => { return Err(generate_error(operator.line, "cannot apply '-' on non-numbers.")); }
                     }
                 },
                 TokenType::Plus => {
                     match (left_object, right_object) {
                         (Value::Number(left_value), Value::Number(right_value)) => 
                         {
-                            return Some(Value::Number(left_value + right_value));
+                            return Ok(Value::Number(left_value + right_value));
                         },
                         (Value::Str(left_value), Value::Str(right_value)) => 
                         {
-                            return Some(Value::Str(format!("{}{}", left_value, right_value)));
+                            return Ok(Value::Str(format!("{}{}", left_value, right_value)));
                         },
-                        (_, _) => { return None; }
+                        (_, _) => { return Err(generate_error(operator.line, "'+' operator must be applied on numbers or strings.")); }
                     }
                 },
                 TokenType::Slash => {
                     match (left_object, right_object) {
                         (Value::Number(left_value), Value::Number(right_value)) => 
                         {
-                            return Some(Value::Number(left_value / right_value));
+                            if right_value == 0.0 {
+                                return Err(generate_error(operator.line, "cannot divide by 0."));
+                            }
+                            return Ok(Value::Number(left_value / right_value));
                         },
-                        (_, _) => { return None; }
+                        (_, _) => { return Err(generate_error(operator.line, "'/' operator must be applied on numbers.")); }
                     }
                 },
                 TokenType::Star => {
                     match (left_object, right_object) {
                         (Value::Number(left_value), Value::Number(right_value)) => 
                         {
-                            return Some(Value::Number(left_value * right_value));
+                            return Ok(Value::Number(left_value * right_value));
                         },
-                        (_, _) => { return None; }
+                        (_, _) => { return Err(generate_error(operator.line, "'*' operator must be applied on numbers.")); }
                     }
                 },
                 TokenType::Greater => {
                     match (left_object, right_object) {
                         (Value::Number(left_value), Value::Number(right_value)) => 
                         {
-                            return Some(Value::Bool(left_value > right_value));
+                            return Ok(Value::Bool(left_value > right_value));
                         },
-                        (_, _) => { return None; }
+                        (_, _) => { return Err(generate_error(operator.line, "'>' operator must be applied on numbers.")); }
                     }
                 },
                 TokenType::GreaterEqual => {
                     match (left_object, right_object) {
                         (Value::Number(left_value), Value::Number(right_value)) => 
                         {
-                            return Some(Value::Bool(left_value >= right_value));
+                            return Ok(Value::Bool(left_value >= right_value));
                         },
-                        (_, _) => { return None; }
+                        (_, _) => { return Err(generate_error(operator.line, "'>=' operator must be applied on numbers.")); }
                     }
                 },
                 TokenType::Less => {
                     match (left_object, right_object) {
                         (Value::Number(left_value), Value::Number(right_value)) => 
                         {
-                            return Some(Value::Bool(left_value < right_value));
+                            return Ok(Value::Bool(left_value < right_value));
                         },
-                        (_, _) => { return None; }
+                        (_, _) => { return Err(generate_error(operator.line, "'<' operator must be applied on numbers.")); }
                     }
                 },
                 TokenType::LessEqual => {
                     match (left_object, right_object) {
                         (Value::Number(left_value), Value::Number(right_value)) => 
                         {
-                            return Some(Value::Bool(left_value <= right_value));
+                            return Ok(Value::Bool(left_value <= right_value));
                         },
-                        (_, _) => { return None; }
+                        (_, _) => { return Err(generate_error(operator.line, "'<=' operator must be applied on numbers.")); }
                     }
                 },
                 TokenType::BangEqual => {
                     match is_equal(&left_object, &right_object) {
-                        Some(result) => { return Some(Value::Bool(!result)); },
-                        None => { return None; }
+                        Some(result) => { return Ok(Value::Bool(!result)); },
+                        // TODO: error should be reported in is_equal
+                        None => { return Err(generate_error(operator.line, "'!=' operator must be applied on the same types.")); }
                     }
                 }
                 TokenType::EqualEqual => {
                     match is_equal(&left_object, &right_object) {
-                        Some(result) => { return Some(Value::Bool(result)); },
-                        None => { return None; }
+                        Some(result) => { return Ok(Value::Bool(result)); },
+                        // TODO: error should be reported in is_equal
+                        None => { return Err(generate_error(operator.line, "'==' operator must be applied on the same types.")); }
                     }
                 }
-                _ => { return None; }
+                _ => { return Err(generate_error(operator.line, "unknown token found while parsing binary expression.")); }
             }
         },
-        _ => { return None; }
+        _ => { return Err(String::from("Unknown expression type found.")); }
     }
-
-    return None;
 }
 
-fn literal_to_value(literal: &Literal) -> Option<Value> {
+fn literal_to_value(literal: &Literal) -> Result<Value, String> {
     match literal {
-        Literal::Identifier(text) => { Some(Value::Identifier(text.clone())) },
-        Literal::Str(text) => { Some(Value::Str(text.clone())) },
-        Literal::Number(number) => { Some(Value::Number(number.clone())) },
-        Literal::Bool(value) => { Some(Value::Bool(value.clone())) },
-        Literal::Nil => { Some(Value::Nil) },
+        Literal::Identifier(text) => { Ok(Value::Identifier(text.clone())) },
+        Literal::Str(text) => { Ok(Value::Str(text.clone())) },
+        Literal::Number(number) => { Ok(Value::Number(number.clone())) },
+        Literal::Bool(value) => { Ok(Value::Bool(value.clone())) },
+        Literal::Nil => { Ok(Value::Nil) },
     }
 }
 
@@ -192,8 +195,12 @@ fn is_equal(left: &Value, right: &Value) -> Option<bool> {
         {
             return Some(true);
         },
-        (_, _) => { return Some(false); }
+        (_, _) => { return None; }
     }
+}
+
+fn generate_error(line: i32, message: &str) -> String {
+	format!("[line {line}] Error: {message}")
 }
 
 #[derive(Clone, PartialEq)]
